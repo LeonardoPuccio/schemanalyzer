105179ef2e62657ce3f5cc914ee0b4911d190547
Leonardo Puccio <puccio.leonardo@gmail.com>
2018-12-03 20:04:26 +0100

    Update README.md and Cleanup

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # schemanalyzer
-Frequency analyzer of the schema.org classes
+_Frequency analyzer of the schema.org classes in nodejs_
+
+L'app permette di caricare una lista di siti dal file [sitelist.csv](https://github.com/LeonardoPuccio/schemanalyzer/blob/master/sitelist.csv), per ogni sito verrà scaricato l'HTML e fatto un parsing per conteggiare tutte le classi schema.org per ogni formato (microdata, rdfa, json-ld).
+
+Restituisce un json cumulativo con la somma di tutte le classi così formato:
+```json
+{
+  "microdata": {
+    "type": "value",
+    ...
+  },
+  "rdfa": {...},
+  "jsonld": {...}
+}
+```
+## Prerequisiti
+##### - [nodejs](https://nodejs.org/)
+##### - [Git](https://git-scm.com/downloads) _OPZIONALE_
+
+## Installazione dipendenze
+
+_Da terminale o prompt dei comandi_
+```
+$ git clone https://github.com/LeonardoPuccio/schemanalyzer.git schemanalyzer
+$ cd schemanalyzer
+$ npm install
+```
+
+> git è opzionale. È sufficiente scaricare il repository ed estrarlo in una cartella qualsiasi, dalla quale eseguiremo `npm install`
+
+## Avvio
+```
+$ node schemanalyzer
+```
+
+## References
+
+* [schema.org](schema.org) - Schemas for structured data on the Internet.
+* [NodeJS](https://nodejs.org/en/about/) - JavaScript runtime built on Chrome's V8 JavaScript engine.
+* [Web Auto Extractor](https://github.com/indix/web-auto-extractor) - Parse semantically structured information from any HTML webpage.
+* [axios](https://github.com/axios/axios) - Promise based HTTP client for the browser and node.js
--- schemanalyzer.js
+++ schemanalyzer.js
@@ -7,7 +7,6 @@ const fs        = require('fs');
 const readline  = require('readline');
 const axios     = require('axios');
 const WAE       = require('web-auto-extractor').default
-// const fij       = require('./find-in-json'); // Vedi #Unit-Test
 const rl = readline.createInterface({
   input: fs.createReadStream('sitelist.csv'),
   crlfDelay: Infinity
@@ -25,7 +24,6 @@ async function getStructuredData(siteUrls){
   let promises = [];
   let types = JSON.parse(`
   {
-    "metatags": {},
     "microdata": {},
     "rdfa": {},
     "jsonld": {}
@@ -52,56 +50,11 @@ async function getStructuredData(siteUrls){
 
 function assignToJson(types, parsed){
   Object.keys(parsed).forEach(format => {
-    Object.keys(parsed[format]).forEach(type => {
-
-      // Verifico se la classe esiste e la incremento, altrimenti la aggiungo
-      types[format].hasOwnProperty(type) ? types[format][type]++ : types[format][type] = 1;
-
-      // Vecchio metodo
-      // if ( fij.getValues(types, type)[0] > 0 ){
-      //   // EXISTS
-      //   types[format][type]++
-      // } else {
-      //   types[format][type] = 1;
-      // }
-      // ternary operator
-      // ( fij.getValues(types, type)[0] > 0 ) ? types[format][type]++ : types[format][type] = 1;
-    });
+    if (format != 'metatags'){
+      Object.keys(parsed[format]).forEach(type => {
+        // Verifico se la classe esiste e la incremento, altrimenti la aggiungo
+        types[format].hasOwnProperty(type) ? types[format][type]++ : types[format][type] = 1;
+      });
+    }
   });
 }
-
-// // #Unit-Test per esplorare il json ( const fij = require('./find-in-json'); )
-// const schema = JSON.parse(fs.readFileSync('schema.json', 'utf8'));
-// // console.log(schema);
-// // console.log(fij.getValues(schema,'@type').length); // 1613
-// function getClasses(){
-//
-//   let n = 0;
-//   fij.getObjects(schema,'@type','').forEach( type => {
-//     if (type['@type'] == 'rdfs:Class'){
-//       console.log(type['rdfs:label']);
-//       n++;
-//     }
-//   });
-//   console.log(n);
-//
-// }
-// getClasses();
-
-// Metodo alternativo (più elegante) per il loop asyncrono
-// let promises = siteUrls.map(async siteUrl => {
-//   let response = await axios.get(siteUrl)
-//     .then(function (response) {
-//       // handle success
-//       // console.log(response);
-//       parsed = WAE().parse(response.data);
-//       console.log(JSON.stringify(parsed.jsonld));
-//       types.push(Object.keys(parsed.jsonld));
-//       // console.log('types: \n' + types + '\n\n');
-//
-//     })
-//     .catch(function (error) {
-//       // handle error
-//       console.log(error);
-//     })
-// });
